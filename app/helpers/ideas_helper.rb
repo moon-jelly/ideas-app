9c472662460d1ead350504a87a3cbec72f3337f9
Matt Powell <fauxparse@gmail.com>
2017-10-29 13:02:58 +1300

    limit rating display to a sensible number of stars

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,5 +1,5 @@
 module IdeasHelper
-  def star_rating(rating)
-    "★" * rating + "☆" * [5 - rating, 0].max if rating
+  def star_rating(rating, max: 5)
+    "★" * [rating, max].min + "☆" * [max - rating, 0].max if rating
   end
 end
